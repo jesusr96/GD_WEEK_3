f3ceee8a993c868a6bc0f5c98c948f19525d568f
gordoniankid <gordoniankid@gmail.com>
2017-09-12 10:29:43 -0400

    uploaded week 2 folder

---
+++ GD205WK2/Assets/Script/respawn.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class respawn : MonoBehaviour {
+	public float threshold;
+
+	void FixedUpdate () {
+		if (transform.position.y < threshold)
+			transform.position = new Vector3(-11, 7, -8);
+	}
+}
+//From Unity Answers//
